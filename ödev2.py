78e360efb605ca0700c0f39ed002636b591d175a
muratdonmez1 <76389641+muratdonmez1@users.noreply.github.com>
2021-05-27 01:39:51 +0300

    Add files via upload

---
+++ ödev2.py
@@ -0,0 +1,10 @@
+cv1={"ad": "murat", "soyad": "dönmez", "doğum tarihi": "13.02.1999","yetenekler":"[python,solidwork,ansys]","tecrübe":"yok"}
+cv2={"ad": "mert", "soyad": "aktaş", "doğum tarihi": "19.07.1999","yetenekler":"[python,solidwork,ansys]","tecrübe":"f1"}
+cv3={"ad": "halil ibrahim", "soyad": "kölge", "doğum tarihi": "15.07.1996","yetenekler":"[python,solidwork,ansys]","tecrübe":"tein"}
+cv4={"ad": "okan", "soyad": "bayram", "doğum tarihi": "13.02.1899","yetenekler":"[python,solidwork,ansys]","tecrübe":"yok"}
+cv5={"ad": "ebru", "soyad": "dönmez", "doğum tarihi": "13.02.1999","yetenekler":"[python,solidwork,ansys]","tecrübe":"yok"}
+print(cv1)
+print(cv2)
+print(cv3)
+print(cv4)
+print(cv5)
